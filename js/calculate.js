03c926ca4719379b6c75df1335d6b18b3b6f1d9f
Ankan112 <ankanhalder112@gmail.com>
2022-08-23 16:06:47 +0600

    function update

--- js/calculate.js
+++ js/calculate.js
@@ -8,8 +8,6 @@ function addPlayers(playersBtn, player) {
             li.appendChild(document.createTextNode(playerName));
             list.appendChild(li);
             const button = document.getElementById(playersBtn);
-
-            // ✅ Set the disabled attribute
             button.setAttribute('disabled', '');
         }
         else {
@@ -17,7 +15,6 @@ function addPlayers(playersBtn, player) {
         }
     })
 }
-console.log('borald;lsj')
 addPlayers('player-btn-1', 'player-1')
 addPlayers('player-btn-2', 'player-2')
 addPlayers('player-btn-3', 'player-3')
@@ -26,27 +23,31 @@ addPlayers('player-btn-5', 'player-5')
 addPlayers('player-btn-6', 'player-6')
 
 document.getElementById('calculate').addEventListener('click', function () {
-    const perPlayerCost = parseFloat(document.getElementById('per-player-cost').value);
+    const perPlayerCostInput = document.getElementById('per-player-cost');
+    const perplayerCostText = perPlayerCostInput.value;
+    const perPlayerCost = parseFloat(perplayerCostText);
     const totalPlayers = document.getElementsByTagName('li').length;
     const totalPlayersCost = totalPlayers * perPlayerCost;
     document.getElementById('player-expenses').innerText = totalPlayersCost;
+    perPlayerCostInput.value = '';
 
 })
-
 
 document.getElementById('calculate-total').addEventListener('click', function () {
     // const playersCost = document.getElementById('player-expenses');
     const playersCostTotal = parseFloat(document.getElementById('player-expenses').innerText);
-    let managerCostText = document.getElementById('manager-cost').value
+    let managerCostInput = document.getElementById('manager-cost');
+    const managerCostText = managerCostInput.value;
     let managerCost = parseFloat(managerCostText);
-    let coachCostText = document.getElementById('coach-cost').value
+    let coachCostInput = document.getElementById('coach-cost');
+    const coachCostText = coachCostInput.value;
     let coachCost = parseFloat(coachCostText);
-
+    console.log(typeof (coachCost))
     const allCost = playersCostTotal + managerCost + coachCost;
     document.getElementById('total').innerText = allCost;
     // const currentTotalText = total.innerText;
     // const currentTotal = parseFloat(currentTotalText);
     // total.innerText = allCost;
-    managerCostText = '';
-    coachCostText = '';
+    managerCostInput.value = '';
+    coachCostInput.value = '';
 })
